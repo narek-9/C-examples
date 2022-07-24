882e9ad802d096fcb23a5b94c5cfe0bfb601c6e1
Narek <narekazibekyan5@gmail.com>
2022-07-24 23:31:32 +0400

    Ex 0.

---
+++ ex 0/Program.cs
@@ -0,0 +1,6 @@
+﻿// Вывести квадрат числа
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int square = num * num;
+Console.WriteLine($"Квадрат числа {num} = {square}");
