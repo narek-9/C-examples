b2f23920d57ff6cd88f8ca57807f702794903210
Narek <narekazibekyan5@gmail.com>
2022-07-25 01:56:42 +0400

    Ex 4.

---
+++ ex 4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Найти максимальное из трех чисел
+
+Random rnd = new Random();
+int num1 = rnd.Next(1, 100);
+int num2 = rnd.Next(1, 100);
+int num3 = rnd.Next(1, 100);
+
+Console.WriteLine($"{num1}, {num2}, {num3}");
+
+if (num1 > num2 && num1 > num3)
+{
+    Console.WriteLine($"Максимальное число = {num1}");
+}
+
+if (num2 > num1 && num2 > num3)
+{
+    Console.WriteLine($"Максимальное число = {num2}");
+}
+
+if (num3 > num1 && num3 > num2)
+{
+    Console.WriteLine($"Максимальное число = {num3}");
+}
