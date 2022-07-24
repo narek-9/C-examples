210865c85fe7022bff969fb37e4f0f1947a54476
Narek <narekazibekyan5@gmail.com>
2022-07-24 23:48:34 +0400

    Ex 2.

---
+++ ex 2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Даны два числа. Показать большее и меньшее число
+
+Console.WriteLine("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine($"{num1} - большее число, {num2} - меньшее число.");
+}
+
+else if (num2 > num1)
+{
+    Console.WriteLine($"{num2} - большее число, {num1} - меньшее число.");
+
+}
+
+if (num1 == num2)
+{
+    Console.WriteLine("Числа равны.");
+}
