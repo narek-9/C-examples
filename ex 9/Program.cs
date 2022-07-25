21e36643eaeb5b4955a12334096e538a999c45f7
Narek <narekazibekyan5@gmail.com>
2022-07-25 22:48:22 +0400

    Ex 9.

---
+++ ex 9/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+Console.WriteLine("Введите трёхзначное число: ");
+
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine(num);
+
+int result = num % 10;
+Console.WriteLine($"Последняя цифра числа = {result}");
+
