14a24d76468c9f3f2b38fdbdd3bdf0873e53e320
Narek <narekazibekyan5@gmail.com>
2022-07-25 16:59:42 +0400

    Ex 6.

---
+++ ex  6/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить является ли число чётным
+
+Console.WriteLine("Введите число։ ");
+int num = int.Parse(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.WriteLine($"Число {num} - чётное");
+}
+
+else
+{
+    Console.WriteLine($"Число {num} - нечётное");
+}
