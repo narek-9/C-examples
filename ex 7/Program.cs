50330b3f281a0a62648fe1e9cba3a9768fc1ba73
Narek <narekazibekyan5@gmail.com>
2022-07-25 17:17:50 +0400

    Ex 7.

---
+++ ex 7/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать числа от -N до N
+
+Console.WriteLine("Введите отрицательное число: ");
+int number = int.Parse(Console.ReadLine());
+int i = number;
+
+while (i <= -number)
+{
+    Console.Write($"{number}, ");
+    number++;
+}
