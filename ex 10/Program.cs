8597a4a634fbe283ac5b568138e674867eaa2155
Narek <narekazibekyan5@gmail.com>
2022-07-25 22:58:32 +0400

    Ex 10.

---
+++ ex 10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.WriteLine("Введите трёхзначное число: ");
+int number = int.Parse(Console.ReadLine());
+if (number > 999 || number < 100)
+{
+    Console.WriteLine("ВВЕДИТЕ ТРЁХЗНАЧНОЕ ЧИСЛО!!!");
+}
+else
+{
+    int result = number / 10 % 10;
+    Console.WriteLine();
+    Console.WriteLine($"Вторая цифра числа = {result}");
+}
