572eea8ba69328ceaaca63c664aaf455c45d43f8
Narek <narekazibekyan5@gmail.com>
2022-08-24 01:28:23 +0400

    Ex 13.

---
+++ ex 13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Random rnd = new Random();
+int num = rnd.Next(1, 1000);
+Console.WriteLine("Введите число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+Console.WriteLine($"Рандомное число = {num}.");
+Console.WriteLine($"Заданное число = {num2}.");
+
+if (num % num2 != 0)
+    Console.WriteLine("Рандомное число не кратно заданному.");
+else
+    Console.WriteLine("Рандомное число кратно заданному.");
