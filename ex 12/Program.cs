8d507ae2e4e656ff2d8de924d7393ccfce49cf95
Narek <narekazibekyan5@gmail.com>
2022-08-24 01:01:58 +0400

    Ex 12.

---
+++ ex 12/Program.cs
@@ -0,0 +1,5 @@
+﻿// Удалить вторую цифру трёхзначного числа
+Random rnd = new Random();
+int num = rnd.Next(100, 1000);
+Console.WriteLine($"Рандомное число = {num}.");
+Console.WriteLine($"{num / 100}" + num % 10);
