ad89943b36b2827c0c62f91c95df712b642dbf3f
Narek <narekazibekyan5@gmail.com>
2022-08-27 01:21:14 +0400

    Ex 20.

---
+++ ex 20/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+Console.WriteLine("Введите номер четверти: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 1)
+    Console.WriteLine("Диапазон 1-ой четверти: x > 0 и y > 0.");
+if (number == 2)
+    Console.WriteLine("Диапазон 2-ой четверти: x < 0 и y > 0.");
+if (number == 3)
+    Console.WriteLine("Диапазон 3-ой четверти: x < 0 и y < 0.");
+if (number == 4)
+    Console.WriteLine("Диапазон 4-ой четверти: x > 0 и y < 0.");
+if (number < 1 | number > 4)
+    Console.WriteLine("Такой четверти нет!");
