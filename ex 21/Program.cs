37900a8df95b24dfb59a40804833ebb44b0d1bcb
Narek <narekazibekyan5@gmail.com>
2022-08-29 12:21:34 +0400

    Ex 21.

---
+++ ex 21/Program.cs
@@ -0,0 +1,18 @@
+﻿// Программа проверяет пятизначное число на палиндромом. 
+
+Random rnd = new Random();
+int num = rnd.Next(10000, 100000);
+Console.WriteLine($"Число = {num}.");
+string str = Convert.ToString(num);
+string[] array = new string[str.Length];
+for (int i = 0; i < str.Length; i++)
+{
+    array[i] = str[i].ToString();
+}
+if (array[0] == array[4] & array[1] == array[3])
+{
+    Console.WriteLine($"{num} - палиндром.");
+}
+else
+    Console.WriteLine($"{num} - не палиндром.");
+
