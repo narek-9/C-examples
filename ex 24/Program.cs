073fbe7562c598fea4acc9ee892e4973b27631c5
Narek <narekazibekyan5@gmail.com>
2022-08-30 15:22:22 +0400

    Ex 24(1).

--- ex 24/Program.cs
+++ ex 24/Program.cs
@@ -3,7 +3,7 @@
 Console.WriteLine("Введите число N: ");
 int N = Convert.ToInt32(Console.ReadLine());
 
-for (int i = 1; i < N; i++)
+for (int i = 1; i <= N; i++)
 {
     Console.WriteLine($"{i}^3 = " + i * i * i);
 }
