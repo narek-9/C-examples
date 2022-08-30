b614f6b1b5155aa48a9d608f3da5539b3e8716a1
Narek <narekazibekyan5@gmail.com>
2022-08-30 15:11:49 +0400

    Ex 23.

---
+++ ex 23/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать таблицу квадратов чисел от 1 до N 
+
+Console.WriteLine("Введите число N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= num; i++)
+{
+    Console.WriteLine($"{i} * {i} = " + i * i);
+}
