34eed9a0270d9ff1a872108b7467a84c2df27e2e
Narek <narekazibekyan5@gmail.com>
2022-08-30 15:26:38 +0400

    Ex 25.

---
+++ ex 25/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти сумму чисел от 1 до А 
+
+Console.WriteLine("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+for (int i = 0; i <= A; i++)
+{
+    count += i;
+}
+Console.WriteLine($"Сумма чисел от 1 до A = {count}");
