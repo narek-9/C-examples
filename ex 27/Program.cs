05d04c3a0e2870ec428aeb8fee81510fbb44859e
Narek <narekazibekyan5@gmail.com>
2022-08-31 23:29:09 +0400

    Ex 27.

---
+++ ex 27/Program.cs
@@ -0,0 +1,12 @@
+﻿// Определить количество цифр в числе
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string str = Convert.ToString(num);
+string[] array = new string[str.Length];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = str[i].ToString();
+}
+Console.WriteLine($"Количество цифр в числе {num} = {array.Length}");
+// Console.WriteLine("[" + string.Join(", ", array) + "]");
