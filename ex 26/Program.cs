3b8ebcba97cd4d8854eefa0e4b2a6a88f2e4f019
Narek <narekazibekyan5@gmail.com>
2022-08-31 23:22:02 +0400

    Ex 26.

---
+++ ex 26/Program.cs
@@ -0,0 +1,15 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+Console.WriteLine("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число A: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+
+for (int i = 0; i < B; i++)
+{
+    count *= A;
+}
+
+Console.WriteLine($"Число {A} в степени {B} = {count}");
