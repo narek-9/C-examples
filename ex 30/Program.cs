12b91d3fcef6858b05a28acc8aaf6a652f5c236a
Narek <narekazibekyan5@gmail.com>
2022-09-03 00:29:37 +0400

    Ex 30.

---
+++ ex 30/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+Random rnd = new Random();
+int num = rnd.Next(1, 100);
+Console.WriteLine(num);
+int result = 1;
+if (num % 2 == 0)
+{
+    result = num * num * num;
+    Console.WriteLine($"Куб числа {num} = {result}");
+}
+else
+{
+    Console.WriteLine($"Число {num} - нечётное.");
+}
