4ab56f9f4b0dbe74f5c6dfac7bbf92ce9dcc20ff
Narek <narekazibekyan5@gmail.com>
2022-09-03 00:19:19 +0400

    Ex 29.

---
+++ ex 29/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int N1 = N;
+int result = 1;
+do
+{
+    result = result * N;
+    N--;
+} while (N != 0);
+
+Console.WriteLine($"Произведение чисел от 1 до {N1} = {result}");
