21001cc29445ce8737d66e2784c2f007785e3901
Narek <narekazibekyan5@gmail.com>
2022-09-02 23:39:35 +0400

    Ex 28.

---
+++ ex 28/Program.cs
@@ -0,0 +1,25 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string str = num.ToString();
+string[] array = new string[str.Length];
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = str[i].ToString();
+}
+for (int i = 0; i < array.Length; i++)
+{
+    count = count + Convert.ToInt32(array[i]);
+}
+
+Console.WriteLine($"Сумма цифр числа {num} = {count}");
+
+
+
+
+
+
+
