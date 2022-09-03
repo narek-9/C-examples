27feed80105f45c6d64ec84f9d610d9524c64fc4
Narek <narekazibekyan5@gmail.com>
2022-09-03 22:42:47 +0400

    Ex 32.

---
+++ ex 32/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран 
+
+Random rnd = new Random();
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(0, 2);
+}
+
+Console.WriteLine("[" + string.Join(", ", array) + "]");
