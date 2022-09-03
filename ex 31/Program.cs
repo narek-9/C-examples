37804fea9d79c8785f44cd94d5ea16128b9ea1ba
Narek <narekazibekyan5@gmail.com>
2022-09-03 22:38:44 +0400

    Ex 31.

---
+++ ex 31/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задать массив из 8 элементов и вывести их на экран
+
+Random rnd = new Random();
+int[] array = new int[8];
+Console.WriteLine("Введите значения массива: ");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("[" + string.Join(", ", array) + "]");
