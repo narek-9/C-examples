5879f154d45829e09893aeaa23dd305681cebf24
Narek <narekazibekyan5@gmail.com>
2022-09-04 13:38:59 +0400

    Ex 34.

---
+++ ex 34/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу замену элементов массива на противоположные
+
+Console.WriteLine("Введите размер масиива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значения масиива: ");
+int[] array = new int[size];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = -array[i];
+}
+Console.WriteLine("[" + string.Join(", ", array) + "]");
