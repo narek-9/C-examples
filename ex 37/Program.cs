310963ec761b86d10722e79cbb490d392252871a
Narek <narekazibekyan5@gmail.com>
2022-09-06 18:45:24 +0400

    Ex 37.

---
+++ ex 37/Program.cs
@@ -0,0 +1,36 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+Random rnd = new Random();
+int[] array = new int[123];
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(1, 1000);
+    if (array[i] > 9 & array[i] < 100)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine("[" + String.Join(", ", array) + "]");
+Console.WriteLine();
+Console.WriteLine($"Количество элементов из отрезка [10,99] = {count}");
+
+// Решение со значениями в масииве до 123
+
+// Random rnd = new Random();
+// int[] array = new int[rnd.Next(1, 40)];
+// int count = 0;
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = rnd.Next(1, 124);
+//     if (array[i] > 9 & array[i] < 100)
+//     {
+//         count++;
+//     }
+// }
+
+// Console.WriteLine("[" + string.Join(", ", array) + "]");
+// Console.WriteLine($"Количество элементов из отрезка [10,99] = {count}");
