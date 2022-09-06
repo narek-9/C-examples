0f27515cc9f936d0505a502aee1f465477aab52f
Narek <narekazibekyan5@gmail.com>
2022-09-06 20:10:26 +0400

    Ex 39.

---
+++ ex 39/Program.cs
@@ -0,0 +1,23 @@
+﻿// Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+Random rnd = new Random();
+int[] array = new int[rnd.Next(1, 20)];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(1, 10);
+}
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+
+int result = 0;
+int n = array.Length - 1;
+for (int i = 0; i < array.Length; i++)
+{
+    if (i > n)
+    {
+        break;
+    }
+
+    result = (array[i] * array[n]);
+    Console.WriteLine($"{array[i]} * {array[n]} = {result}");
+    n--;
+}
