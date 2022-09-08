12ce5fb90f007161bfd78b75f74a171cbf59f4d8
Narek <narekazibekyan5@gmail.com>
2022-09-09 00:30:03 +0400

    Ex 41.

---
+++ ex 41/Program.cs
@@ -0,0 +1,17 @@
+﻿// Выяснить являются ли три числа сторонами треугольник
+
+Console.WriteLine("Введите первую сторону: ");
+Console.WriteLine("Введите первую сторону: ");
+Console.WriteLine("Введите первую сторону: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if ((num1 + num2 <= num3) | (num1 + num3 <= num2) | (num2 + num3 <= num1))
+{
+    Console.WriteLine($"Числа {num1}, {num2} и {num3} - не стороны треугольника.");
+}
+else
+    Console.WriteLine($"Числа {num1}, {num2} и {num3} - стороны треугольника.");
+
+
