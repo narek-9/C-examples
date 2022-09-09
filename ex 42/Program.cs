e88fb2fcef2ff04c38779a4ec6f0fb7232a99ac0
Narek <narekazibekyan5@gmail.com>
2022-09-10 03:30:04 +0400

    Ex 42.

---
+++ ex 42/Program.cs
@@ -0,0 +1,13 @@
+﻿// Определить сколько чисел больше 0 введено с клавиатуры 
+Console.Write("Введите числа через пробел: ");
+int[] array = Console.ReadLine().Split(' ').Select(e => Convert.ToInt32(e)).ToArray();
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Количество чисел > 0 = {count}");
