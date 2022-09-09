35fc2f837239ed9f9bd413de362acdfb7813714a
Narek <narekazibekyan5@gmail.com>
2022-09-10 03:59:38 +0400

    Ex 45.

---
+++ ex 45/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать числа Фибоначчи
+
+Console.WriteLine("Сколько чисел Фибоначчи нужно напечатать? ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[num];
+for (int i = 2; i < array.Length; i++)
+{
+    array[0] = 0;
+    array[1] = 1;
+    array[i] = array[i - 1] + array[i - 2];
+}
+
+Console.WriteLine("[" + string.Join(", ", array) + "]");
