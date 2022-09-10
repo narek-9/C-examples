ec3886e09a58526867595fdd5f4e8cb17ffaa8a2
Narek <narekazibekyan5@gmail.com>
2022-09-10 04:13:48 +0400

    Ex 47.

---
+++ ex 47/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу копирования массива
+
+Console.Write("Введите значения массива через пробел: ");
+int[] array = Console.ReadLine().Split(' ').Select(e => Convert.ToInt32(e)).ToArray();
+int[] array2 = new int[array.Length];
+// Array.Copy(array, array2, array2.Length);
+for (int i = 0; i < array.Length; i++)
+{
+    array2[i] = array[i];
+}
+
+Console.WriteLine("Первоначальный массив: " + "[" + string.Join(", ", array) + "]");
+Console.WriteLine("Скопированный массив: " + "[" + string.Join(", ", array2) + "]");
